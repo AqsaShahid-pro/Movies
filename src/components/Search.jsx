df663fc227095d4c29575e357bd24e4851edf3fc
AqsaNtigra <139318588+AqsaNtigra@users.noreply.github.com>
2025-04-30 21:14:55 +0500

    Movie App

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+    return (
+        <div className='search'>
+            <div>
+                <img src='search.svg' />
+
+                <input type='text' placeholder='Search a Movie' value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)} />
+            </div>
+        </div>
+    )
+}
+
+export default Search
